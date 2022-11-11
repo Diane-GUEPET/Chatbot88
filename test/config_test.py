9cea47003a15b6048e2329a34b2561c378a9123e
Diane-GUEPET <dianetchawa@gmail.com>
2022-11-11 11:30:05 +0100

    update des clés

--- test/config_test.py
+++ test/config_test.py
@@ -9,11 +9,11 @@ class TestConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword")
-    LUIS_APP_ID = os.environ.get("LuisAppId")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey")
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westeurope.api.cognitive.microsoft.com")
-    LUIS_API_ENDPOINT = os.environ.get("LuisAPIEndPoint", "https://myflymebot.cognitiveservices.azure.com")
+    APP_ID = os.environ.get("APP_ID", "")
+    APP_PASSWORD = os.environ.get("APP_PASSWORD", "")
+    LUIS_APP_ID = os.environ.get("LUISAPPID", "")
+    LUIS_API_KEY = os.environ.get("LUISAPIKEY", "")
+    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME", "")
+    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT", "")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey")
+        "APPINSIGHTSINSTRUMENTATIONKEY", "")
