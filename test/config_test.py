a6eab7cde6061ce9e6d9af65752bce3ab0e9152b
Diane-GUEPET <dianetchawa@gmail.com>
2022-11-11 11:35:37 +0100

    update test

--- test/config_test.py
+++ test/config_test.py
@@ -9,11 +9,11 @@ class TestConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("APP_ID", "")
-    APP_PASSWORD = os.environ.get("APP_PASSWORD", "")
-    LUIS_APP_ID = os.environ.get("LUISAPPID", "")
-    LUIS_API_KEY = os.environ.get("LUISAPIKEY", "")
-    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME", "")
-    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT", "")
+    APP_ID = os.environ.get("APP_ID")
+    APP_PASSWORD = os.environ.get("APP_PASSWORD")
+    LUIS_APP_ID = os.environ.get("LUISAPPID")
+    LUIS_API_KEY = os.environ.get("LUISAPIKEY")
+    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME")
+    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "APPINSIGHTSINSTRUMENTATIONKEY", "")
+        "APPINSIGHTSINSTRUMENTATIONKEY")
