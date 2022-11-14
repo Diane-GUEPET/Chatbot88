908f0f16697a38f4f73cce52e36a21d5506f09a6
Diane-GUEPET <dianetchawa@gmail.com>
2022-11-14 15:18:15 +0100

    Updating Config.py

--- config.py
+++ config.py
@@ -7,23 +7,18 @@ import os
 
 
 class DefaultConfig:
-    """Bot Configuration """
-
-    ## Azure Bot Service ##
+    """Configuration for the bot."""
 
     PORT = 8000
-    APP_ID = os.environ.get("APP_ID", "")
-    APP_PASSWORD = os.environ.get("APP_PASSWORD", "")
-    
-    ## Luis Service ##
-    LUIS_APP_ID = os.environ.get("LUISAPPID", "")
-    LUIS_API_KEY = os.environ.get("LUISAPIKEY", "")
-    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME", "")
-    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT", "")
-
-    ## App Insight Service ##
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "APPINSIGHTSINSTRUMENTATIONKEY", "")
+        "AppInsightsInstrumentationKey", ""
+    )
     
     
    
--- test/config.py
+++ test/config.py
@@ -12,18 +12,23 @@ class DefaultConfig:
     ## Azure Bot Service ##
 
     PORT = 8000
-    APP_ID = os.environ.get("APP_ID", "49c63f45-161d-40ad-bf96-5839d2e7e595")
-    APP_PASSWORD = os.environ.get("APP_PASSWORD", "4f5a8008-ba00-476d-b552-328c7242cdb6")
+    APP_ID = os.environ.get("APP_ID", "")
+    APP_PASSWORD = os.environ.get("APP_PASSWORD", "")
     
     ## Luis Service ##
-    LUIS_APP_ID = os.environ.get("LUISAPPID", "7a937442-e53c-4ad8-8bd6-db1e766c721c")
-    LUIS_API_KEY = os.environ.get("LUISAPIKEY", "5fbcaf962489475a84324f147845851b")
-    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME", "westeurope.api.cognitive.microsoft.com")
-    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT", "https://myflymebot.cognitiveservices.azure.com/")
+    LUIS_APP_ID = os.environ.get("LUISAPPID", "")
+    #"7a937442-e53c-4ad8-8bd6-db1e766c721c"
+    LUIS_API_KEY = os.environ.get("LUISAPIKEY", "")
+    #"5fbcaf962489475a84324f147845851b"
+    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME", "")
+    #"westeurope.api.cognitive.microsoft.com"
+    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT", "")
+    #"https://myflymebot.cognitiveservices.azure.com/"
 
     ## App Insight Service ##
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "APPINSIGHTSINSTRUMENTATIONKEY", "9d08958a-bc12-453b-80a2-10d03c0934fe")
+        "APPINSIGHTSINSTRUMENTATIONKEY","")
+        # "9d08958a-bc12-453b-80a2-10d03c0934fe"
     
     
    
