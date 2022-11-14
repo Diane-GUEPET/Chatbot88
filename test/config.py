10aa0a59348648c0ce57f054e57e2fa1b757df04
Diane-GUEPET <dianetchawa@gmail.com>
2022-11-14 10:45:20 +0100

    config test reupdate

--- test/config.py
+++ test/config.py
@@ -16,14 +16,14 @@ class DefaultConfig:
     APP_PASSWORD = os.environ.get("APP_PASSWORD", "")
     
     ## Luis Service ##
-    LUIS_APP_ID = os.environ.get("LUISAPPID", "")
-    LUIS_API_KEY = os.environ.get("LUISAPIKEY", "")
-    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME", "")
-    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT", "")
+    LUIS_APP_ID = os.environ.get("LUISAPPID", "2805db0b-a3d3-4708-9fd5-8b2388702701")
+    LUIS_API_KEY = os.environ.get("LUISAPIKEY", "ee900aafeadf4cbe8a2b9571984bc2bb")
+    LUIS_API_HOST_NAME = os.environ.get("LUIS_API_HOST_NAME", "westeurope.api.cognitive.microsoft.com")
+    LUIS_API_ENDPOINT = os.environ.get("LUIS_API_ENDPOINT", "https://myflymebot.cognitiveservices.azure.com/")
 
     ## App Insight Service ##
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "APPINSIGHTSINSTRUMENTATIONKEY", "")
+        "APPINSIGHTSINSTRUMENTATIONKEY", "9d08958a-bc12-453b-80a2-10d03c0934fe")
     
     
    
